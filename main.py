9dba21955681d2ede0daa03b4acdab2f6b6d039a
burhansaydin <burhasaydin@gmail.com>
2020-12-01 17:57:27 +0200

    edit

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import turtle as t
+from turtle import Screen
+import random
+color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40),
+              (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71),
+              (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90),
+              (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209),
+              ]
+t.colormode(255)
+tim = t.Turtle()
+tim.width()
+tim.speed(0)
+tim.setheading(220)
+tim.pu()
+tim.hideturtle()
+tim.forward(350)
+tim.setheading(0)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+tim.dot(20, random.choice(color_list))
+for i in range(10):
+    tim.dot(20, random.choice(color_list))
+    for i in range(10):
+        tim.forward(50)
+        tim.dot(20, random.choice(color_list))
+    tim.left(90)
+    tim.forward(50)
+    tim.left(90)
+    tim.forward(500)
+    tim.setheading(0)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+screen = t.Screen()
+screen.exitonclick()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
